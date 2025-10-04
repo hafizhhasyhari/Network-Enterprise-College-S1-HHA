8b2e8a6e5418317b7d130f93146135e571f714f8
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-10-04 23:42:56 +0700

    Create week-001-port_scanner.py

---
+++ Materi-Pertemuan 001/week-001-port_scanner.py
@@ -0,0 +1,17 @@
+# port_scanner.py
+import socket
+
+target = "127.0.0.1"  # Localhost
+ports = [22, 80, 443]  # Daftar port yang ingin dicek
+
+print(f"Scanning ports on {target}...")
+
+for port in ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((target, port))
+    if result == 0:
+        print(f"Port {port} is open ✅")
+    else:
+        print(f"Port {port} is closed ❌")
+    sock.close()
